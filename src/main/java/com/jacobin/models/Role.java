a8e48f4fdd8ff1a80ed3ad63faeabaab5fbe5da5
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2023-11-11 22:52:12 +0700

    add entity
    Role.java file
    User.java file

--- src/main/java/com/jacobin/models/Role.java
+++ src/main/java/com/jacobin/models/Role.java
@@ -3,11 +3,19 @@ package com.jacobin.models;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "role")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,7 +24,13 @@ public class Role implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private int roleId;
+	@Id
+    @Column(name = "role_id")
+    private int roleId;
+	
+	@Column(name = "role_name")
 	private String roleName;
+
+	@OneToMany(mappedBy = "role")
 	private List<User> user;
 }
--- src/main/java/com/jacobin/models/User.java
+++ src/main/java/com/jacobin/models/User.java
@@ -2,11 +2,22 @@ package com.jacobin.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "user")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,13 +26,33 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private int userId;
+	@Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    
+    @Column(name = "phone")
 	private String phone;
+    
+    @Column(name = "password")
 	private String password;
-	private String email;
-	private String firstName;
-	private String lastName;
+	
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "date_of_birth")
 	private String dateOfBirth;
+    
+    @Column(name = "address")
 	private String address;
-//	private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
