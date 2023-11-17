df07debbf8dc49923a02b1e185492193a46b8729
Đoàn Thái Sơn <95365239+Bisoo12300@users.noreply.github.com>
2023-11-17 22:56:12 +0700

    add dao - LineItemDB.java file

---
+++ src/main/java/com/jacobin/dao/LineItemDB.java
@@ -0,0 +1,72 @@
+package com.jacobin.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import com.jacobin.models.LineItem;
+
+public class LineItemDB {
+
+	public static void insert(LineItem lineItem) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();        
+        try {
+            em.persist(lineItem);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void update(LineItem lineItem) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();       
+        try {
+            em.merge(lineItem);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void delete(LineItem lineItem) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();        
+        try {
+            em.remove(em.merge(lineItem));
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }       
+    }
+
+    public static LineItem selectLineItemById(int lineItemId) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        String qString = "SELECT l FROM LineItem l " +
+                "WHERE l.lineItemId = :lineItemId";
+        TypedQuery<LineItem> q = em.createQuery(qString, LineItem.class);
+        q.setParameter("lineItemId", lineItemId);
+        try {
+        	LineItem lineItem = q.getSingleResult();
+            return lineItem;
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+}
